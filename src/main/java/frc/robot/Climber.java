6c35f43758da4a70469ade4987f43f96400a5c7b
CjDj4210 <colton.patterson124@gmail.com>
2026-02-21 11:08:37 -0600

    Climber(colton)

---
+++ src/main/java/frc/robot/Climber.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.CoralConstants.*;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Climber extends subsystemBase{
+    private double climbarm = 0.0;
+   
+    private final SparkMax climbarmmoter = new SparkMax(climbarm_ID, MotorType.kBrushless);
+
+    private final SparkMaxConfig climbarmconfig = new SparkMaxConfig();
+
+    private final SparkClosedLoopController climbarmcontroller = climbarmmoter.getClosedloopController();
+
+    public Climbersubsystem(){
+        climbarmconfig
+        .smartCurrentLimit(40)
+        .IdleMode(IdleMode.kBrake)
+        .closedLoop
+        .pid(1,0,0)
+        ;
+        climbarmmoter.configure(climbarmconfig, ResetMode.kRestSafeParameters, PersistMode.KPersistParmeters);
+    }
+    
+}
